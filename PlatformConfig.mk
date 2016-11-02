1c60c5cbe29037918fdca7c7a35b0944d25bf5e8
drakonizer <nikhilpe@gmail.com>
2016-11-03 01:04:17 +0530

    Add initial multirom support for all rhine devices

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -74,4 +74,47 @@ TARGET_SYSTEM_PROP += device/sony/rhine/system.prop
 # SELinux
 BOARD_SEPOLICY_DIRS += device/sony/rhine/sepolicy
 
+# TWRP
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_FSTAB := device/sony/rhine/multirom/twrp.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_HAS_NO_SELECT_BUTTON := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/sony/rhine/multirom/mr_init_devices.c
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := device/sony/rhine/multirom/mr_qcom_overlay.h
+MR_KEXEC_DTB := true
+MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
+MR_FSTAB := device/sony/rhine/multirom/twrp.fstab
+MR_USE_MROM_FSTAB := true
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+MR_PIXEL_FORMAT := "RGBX_8888"
+MR_KEXEC_MEM_MIN := 0x20000000
+MR_NO_KEXEC := 2
+TARGET_RECOVERY_IS_MULTIROM := true
+MR_DPI := hdpi
+MR_DPI_FONT := 340
+
+# Ensure gzip compression for recovery
+BOARD_NEEDS_LZMA_MINIGZIP := false
+
 include device/sony/common/CommonConfig.mk
